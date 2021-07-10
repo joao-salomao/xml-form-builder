a7ea5f21ddf0114372b71ab275f8211ac506fc19
João Salomão <joaosalomao.613@gmail.com>
2021-07-10 17:14:34 -0300

    Adicionando componente de visualização do xml gerado

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import { parse } from 'js2xmlparser'
 import MetaData from './components/MetaData'
 import Questions from './components/Questions'
+import XMLViewer from './components/XMLViewer'
 import { DEFAULT_QUESTION } from './constants'
 
 function App() {
@@ -9,53 +9,11 @@ function App() {
   const [description, setDescription] = useState("DescriçãoDescriçãoDescrição")
   const [questions, setQuestions] = useState([{ ...DEFAULT_QUESTION }])
 
-  const onSubmit = event => {
-    event.preventDefault()
-
-    const obj = {
-      metadata: {
-        title: {
-          '#': title
-        },
-        description: {
-          '#': description
-        },
-      },
-      issues: {
-        question: questions.map(q => {
-          const parsed = {
-            type: {
-              '#': q.type
-            },
-            label: {
-              '#': q.label
-            }
-          }
-
-          if (q.type == 'select') {
-            parsed.option = q.options.map(option => {
-              return {
-                '#': option
-              }
-            })
-          }
-
-          return parsed
-        })
-      }
-    }
-
-    console.log(parse("survey", obj));
-  }
-
-
   return (
     <main>
-      <form className='form' onSubmit={e => onSubmit(e)}>
-        <MetaData title={title} description={description} setTitle={setTitle} setDescription={setDescription} />
-        <Questions questions={questions} setQuestions={setQuestions} />
-        <input className="submit-button" type="submit" value="Submit" />
-      </form>
+      <MetaData title={title} description={description} setTitle={setTitle} setDescription={setDescription} />
+      <Questions questions={questions} setQuestions={setQuestions} />
+      <XMLViewer title={title} description={description} questions={questions} />
     </main >
   );
 }
---
+++ src/components/XMLViewer.js
@@ -0,0 +1,52 @@
+import { useMemo } from 'react'
+import { parse } from 'js2xmlparser'
+import XMLViewerWidget from 'react-xml-viewer'
+
+export default function XMLViewer({ title, description, questions }) {
+    const xml = useMemo(() => parseFormToXML(title, description, questions))
+
+    return <div className="xml-viewer">
+        <XMLViewerWidget xml={xml} />
+    </div>
+}
+
+const parseFormToXML = (title, description, questions) => {
+    return parse("survey", {
+        metadata: parseMetadata(title, description),
+        issues: parseIssues(questions)
+    })
+}
+
+const parseMetadata = (title, description) => {
+    return {
+        title: {
+            '#': title
+        },
+        description: {
+            '#': description
+        },
+    }
+}
+
+const parseIssues = questions => {
+    return {
+        question: questions.map(q => parseQuestion(q))
+    }
+}
+
+const parseQuestion = question => {
+    const result = {
+        type: {
+            '#': question.type
+        },
+        label: {
+            '#': question.label
+        }
+    }
+
+    if (question.type == 'select') {
+        result.option = question.options.map(option => ({ '#': option }))
+    }
+
+    return result
+}
