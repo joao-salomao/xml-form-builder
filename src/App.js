bbdbebdc4fc5490e119c6e9c1a6dc2eae1146267
João Salomão <joaosalomao.613@gmail.com>
2021-07-10 03:06:20 -0300

    Iniciando o parse dos objeto para xml

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import { useState } from 'react'
+import { parse } from 'js2xmlparser'
 import MetaData from './components/MetaData'
 import Questions from './components/Questions'
 
 function App() {
-  const [title, setTitle] = useState("")
-  const [description, setDescription] = useState("")
+  const [title, setTitle] = useState("TítuloTítuloTítulo")
+  const [description, setDescription] = useState("DescriçãoDescriçãoDescrição")
   const [questions, setQuestions] = useState([])
+
+  const onSubmit = event => {
+    event.preventDefault()
+
+    const obj = {
+      survey: {
+        metadata: {
+          title: {
+            '#': title
+          },
+          description: {
+            '#': description
+          },
+        },
+      }
+    }
+
+    console.log(parse("person", obj));
+  }
+
 
   return (
     <div style={{ padding: '30px' }}>
-      <form>
+      <form onSubmit={e => onSubmit(e)}>
         <MetaData title={title} description={description} setTitle={setTitle} setDescription={setDescription} />
         <Questions questions={questions} setQuestions={setQuestions} />
         <input type="submit" />
